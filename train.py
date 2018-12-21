53d7f8e3018f4b18e4dd4c63947a8f7b813e782b
CARVAL Thibaut <thibaut.carval@wavestone.com>
2018-12-21 19:13:33 +0100

    rnn size 256, 2 layers, batch 64, seq 64, epoch 150

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def main():
                         help='learning rate')
     parser.add_argument('--decay_rate', type=float, default=0.97,
                         help='decay rate for rmsprop')
-    parser.add_argument('--output_keep_prob', type=float, default=0.8,
+    parser.add_argument('--output_keep_prob', type=float, default=1.0,
                         help='probability of keeping weights in the hidden layer')
     parser.add_argument('--input_keep_prob', type=float, default=1.0,
                         help='probability of keeping weights in the input layer')
